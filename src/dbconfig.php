fb72d8759382a8cc323bd26aa7558b56b86362e0
Yassir Fri <yassir.fri@um6p.ma>
2023-05-18 21:53:37 +0100

    working on data acess control

--- src/dbconfig.php
+++ src/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
+
 session_start();
 $dsn = 'mysql:host=localhost;dbname=industrialPerformance';
-
 
 $user = $_POST['username'];
 $pass = $_POST['password'];
@@ -10,26 +10,39 @@ $_SESSION['password'] = $pass;
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_EMULATE_PREPARES => false,
-  ];
+];
 
 try {
     $connexion = new PDO($dsn, $user, $pass, $options);
-
-    if($_SESSION['username'] == 'yassir'){
-        header("Location: views/dashboard.php");
-    }
-    else{
-        $username = $_SESSION['username']; 
+    if ($_SESSION['username'] == 'yassir') {
+        $query = "SHOW GRANTS FOR '$user'@'%'";
+        $stmt = $connexion->query($query);
+        if ($stmt) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($row) {
+                $grants = $row["Grants for $user@%"]; // Adjust the column name based on the actual output
+                $keywords = ['delete', 'insert', 'select'];
+                foreach ($keywords as $keyword) {
+                    if (stripos(strtolower($grants), $keyword) !== false) {
+                        $_SESSION[$keyword] = true;
+                    } else {
+                        $_SESSION[$keyword] = false;
+                    }
+                }
+            }  
+        }
+        header("Location: views/AdminDashboard.php");
+    } else {
+        $username = $_SESSION['username'];
         $query = "SHOW GRANTS FOR '$username'@'%'";
         $stmt = $connexion->query($query);
-
         if ($stmt) {
             $privileges = [];
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 $grants = $row["Grants for $username@%"]; // Adjust the column name based on the actual output
                 $privileges[] = $grants;
             }
-            
+
             $_SESSION["user_privileges"] = $privileges;
             var_dump($_SESSION["user_privileges"]);
         } else {
@@ -38,7 +51,9 @@ try {
         header("Location: views/UserDashboard.php");
     }
 } catch (PDOException $e) {
+    session_destroy();
     echo "Connection failed: " . $e->getMessage();
+    header("Location: ./views/Login.php");
 }
 
 ?>
